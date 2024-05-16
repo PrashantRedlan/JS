d96c6ec40aad4641fbb91ae8f77aac4f24c5c2d2
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2024-05-16 10:57:29 +0000

    Operations on operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,8 +17,49 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33;
+// let someNumber = 33;
  
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// *********************** Operations ***********************
+
+// let value = 3;
+// let nagValue = -value;
+// console.log(nagValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " Prashant"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(+true);
+// console.log(true+);
+console.log(+"");
+
+let num1, num2,num3;
+num1 = num2 = num3 = 2 + 2;
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let gameCounter = 100;
+gameCounter++;
+
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
