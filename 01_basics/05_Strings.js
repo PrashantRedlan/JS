af304ed781936e08c10b76d90774c95dd8112803
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2024-05-17 11:03:18 +0000

    Strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,9 @@
+const name = "Prashant";
+const repoCount = 50;
+
+// console.log(name + " "+repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo course is ${repoCount}`);
+
+const gameName = new String("PrashantHT");
+ console.log(gameName[0]);
