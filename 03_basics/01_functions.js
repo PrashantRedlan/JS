32ab7c1b5159a74a6fdd5e65c20245ea95453c54
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2024-05-28 08:09:38 +0000

    Taking parameters in functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+
+
+ function sayMyName(){
+    console.log("P");
+    console.log("R");
+    console.log("A ");
+    console.log("S");
+    console.log("H");
+    console.log("A");
+    console.log("N");
+    console.log("T");
+ }
+
+ sayMyName();
+
+//  function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2);
+//  }
+ function addTwoNumbers(number1, number2){
+    let result = number1 + number2;
+    return result;
+ }
+
+ const result = addTwoNumbers(3,5);
+
+ console.log("Result: ", result);
