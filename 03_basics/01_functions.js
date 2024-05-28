64c52560c92ca1c5a613f4023cc717b1f8eceef1
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2024-05-28 10:02:40 +0000

    Functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -11,16 +11,37 @@
     console.log("T");
  }
 
- sayMyName();
+//  sayMyName();
 
 //  function addTwoNumbers(number1, number2){
 //     console.log(number1 + number2);
 //  }
  function addTwoNumbers(number1, number2){
-    let result = number1 + number2;
-    return result;
+   //  let result = number1 + number2;
+   //  return result;
+   return number1 + number2;
  }
 
  const result = addTwoNumbers(3,5);
 
- console.log("Result: ", result);
+//  console.log("Result: ", result);
+
+// if userName is not passed the it will return undefined in place of username
+ function loginUserMessage(username = "sam"){
+   // if(!undefined){
+   //    console.log("Please enter an username ");
+   //    return;
+   // }
+   if(username === undefined){
+      console.log("Please enter an username ");
+      return;
+   }
+
+   return `${username} just logged in`;
+ }
+
+//  const ans = loginUserMessage("Mohit");
+
+ console.log(loginUserMessage());
+//  console.log(loginUserMessage());
+// console.log(ans);
