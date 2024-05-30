f73b67051d725aabcaee5d29dcff957181a31bf7
Prashant <132148666+PrashantRedlan@users.noreply.github.com>
2024-05-30 07:16:38 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,6 +42,41 @@
 
 //  const ans = loginUserMessage("Mohit");
 
- console.log(loginUserMessage());
+//  console.log(loginUserMessage());
 //  console.log(loginUserMessage());
 // console.log(ans);
+
+
+// Functions
+
+function calculateCartPrice(...num1){
+  return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user = {
+  username: "Prashant",
+  price: 199
+}
+
+function handleObject(anyObject){
+  // console.log(`UserName is ${anyObject.username} and price is ${anyObject.price} `);
+}
+
+// handleObject(user);
+
+handleObject({
+  username: "Sam",
+  price: 399
+})
+
+const myNewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,400,500,1000]));
